7ec6d31b33f3c5f86fb1f1a42d824b52625880bf
Student <student@RU-005.local>
2016-02-15 17:36:19 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+
+fs.readFile('data.txt', function(err, data))
+
+
+
+// Requires \\
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Create Express App Object \\
+var app = express();
+
+// Application Configuration \\
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+// Routes \\
+app.get('/', function(req, res){
+  res.header('Content-Type', 'text/html')
+  res.send(fileContents)
+});
+
+// Creating Server and Listening for Connections \\
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+})
+
+
+
