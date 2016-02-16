8537494597b3c64206d767539ecef277b3dbf7cd
Student <student@RU-005.local>
2016-02-16 14:20:04 -0700

    done for now

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 var fs = require('fs');
 
-fs.readFile('data.txt', function(err, data))
+
 
 
 
 // Requires \\
 var express = require('express');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
 
 // Create Express App Object \\
 var app = express();
 
 // Application Configuration \\
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 // Routes \\
-app.get('/', function(req, res){
-  res.header('Content-Type', 'text/html')
-  res.send(fileContents)
-});
+app.get('/:filename', function(req, res){
+	fs.readFile('req.params.filename', function(err, body){
+		body = req.params.filename
+	})
+	res.header('Content-Type', 'text/html')
+ 	res.send(body)
+})
+
+// fs.readFile('req.params.filename', function(err, data){
+// 	app.get('/:filename', function(req, res){
+//   		res.header('Content-Type', 'text/html')
+//  		res.send(data)
+// 	})
+// });
+
 
 // Creating Server and Listening for Connections \\
 var port = 3000
